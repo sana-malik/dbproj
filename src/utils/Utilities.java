410d424bbd548109210ed0b58169f3c3fc95cf26
jetzm <jetzm81@gmail.com>
2013-01-03 22:34:09 -0500

    Added methods for object saving/loading.

--- src/utils/Utilities.java
+++ src/utils/Utilities.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.io.*;
+
 public class Utilities {
 	
 	public static String normalizeText(String text) {
@@ -7,4 +9,29 @@ public class Utilities {
 		normal.replaceAll("\\W", "");  // remove punctuation and spaces
 		return normal;
 	}
+	
+	// Serializes and saves an object
+	public static void saveObject( Object obj, String filename ) {
+		try {
+			FileOutputStream f = new FileOutputStream( filename );
+			ObjectOutputStream o = new ObjectOutputStream(f);
+			o.writeObject( obj );
+			o.close();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	// Loads a serialized object
+	public static Object loadObject( String filename ) {
+		try {
+			FileInputStream f = new FileInputStream( filename );
+			ObjectInputStream o = new ObjectInputStream(f);
+			return o.readObject();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return null;
+		}
+	}
+	
 }
