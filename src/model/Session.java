677b40b96d32db8028a030a2aed8f546c4115505
jetzm <jetzm81@gmail.com>
2013-01-03 22:33:28 -0500

    Added some member variables.

--- src/model/Session.java
+++ src/model/Session.java
@@ -2,6 +2,7 @@ package model;
 
 public class Session {
 	private String teamId;
+	private int time_since_last_answer;
 	
 	public Session(String teamId) {
 		this.teamId = teamId;
--- src/model/Team.java
+++ src/model/Team.java
@@ -4,4 +4,9 @@ public class Team {
 	private String name;
 	private int size;
 	private String bio;
+	
+	private boolean killed;
+	private int district;
+	
+	private String death_video_path;
 }
