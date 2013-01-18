af3b7cbe5a69d9be1ec87e7a8331f5557aed0bdb
jetzm <jetzm81@gmail.com>
2013-01-18 12:05:19 -0500

    Updated Session data to match design.

--- src/model/Session.java
+++ src/model/Session.java
@@ -1,11 +1,24 @@
 package model;
 
+import java.sql.Time;
+import java.util.ArrayList;
+
 public class Session {
-	private int time_since_last_answer;
+	private String teamId;
+	
+	private Time start_time;
+	private int elapsed_time;
+	
 	private Team active_team;
 	
+	private ArrayList<Team> team_list;
+	private ArrayList<Puzzle> active_puzzle_list;
+	private ArrayList<Resource> unlocked_resource_list;
+	
 	public Session(String teamId) {
+		team_list = new ArrayList<Team>();
 		active_team = new Team(teamId);
+		team_list.add(active_team);
 	}
 	
 	public String getTeamId() {
