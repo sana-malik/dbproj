a14dfd966a095bab9d3ccc5f94a27e41411b525c
jetzm <jetzm81@gmail.com>
2013-01-24 14:11:10 -0500

    Added some XML testing code

---
+++ src/utils/XMLReader.java
@@ -0,0 +1,54 @@
+package utils;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+ 
+public class XMLReader {
+ 
+	public static void readFile(String filename) {
+ 
+	  try {
+ 
+		File fXmlFile = new File("C:\\Users\\Phil\\Documents\\GitHub\\famine-game\\data\\file.xml");
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(fXmlFile);
+		doc.getDocumentElement().normalize();
+ 
+		System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+		NodeList nList = doc.getElementsByTagName("staff");
+		System.out.println("-----------------------");
+ 
+		for (int temp = 0; temp < nList.getLength(); temp++) {
+ 
+		   Node nNode = nList.item(temp);
+		   if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+ 
+		      Element eElement = (Element) nNode;
+ 
+		      System.out.println("First Name : " + getTagValue("firstname", eElement));
+		      System.out.println("Last Name : " + getTagValue("lastname", eElement));
+	              System.out.println("Nick Name : " + getTagValue("nickname", eElement));
+		      System.out.println("Salary : " + getTagValue("salary", eElement));
+ 
+		   }
+		}
+	  } catch (Exception e) {
+		e.printStackTrace();
+	  }
+  }
+ 
+  private static String getTagValue(String sTag, Element eElement) {
+	NodeList nlList = eElement.getElementsByTagName(sTag).item(0).getChildNodes();
+ 
+        Node nValue = (Node) nlList.item(0);
+ 
+	return nValue.getNodeValue();
+  }
+ 
+}
