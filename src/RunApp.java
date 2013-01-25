66ee0a94ab3d3759b33c2a534549661bac62aad6
Philip Dasler <philip.dasler@gmail.com>
2013-01-25 00:17:53 -0500

    Added location list for testing.

--- src/RunApp.java
+++ src/RunApp.java
@@ -1,9 +1,17 @@
+import java.util.ArrayList;
+
+import model.Location;
 import model.Session;
+import utils.XMLReader;
 import view.MainWindow;
 
 
 public class RunApp {
 	public static void main(String[] args) {
+		
+		ArrayList<Location> locations = new ArrayList<Location>();
+		locations.add( XMLReader.readLocation("data\\location1.xml") );
+		
 		Session session = new Session("01");
 		MainWindow mainWindow = new MainWindow(session);
 	}
