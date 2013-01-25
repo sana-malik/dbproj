d8ed15c2918cf956ccb64f94b79c566bbb546748
Philip Dasler <philip.dasler@gmail.com>
2013-01-25 00:17:07 -0500

    Added constructor.

--- src/model/Hint.java
+++ src/model/Hint.java
@@ -11,6 +11,17 @@ public class Hint {
 	
 	private int max_fan_cost;
 	private int min_fan_cost;
-
+	
+	
+	public Hint(String text, int minutes_till_available,
+			int minutes_till_min_cost, int max_fan_cost,
+			int min_fan_cost) {
+		super();
+		this.text = text;
+		this.minutes_till_available = minutes_till_available;
+		this.minutes_till_min_cost = minutes_till_min_cost;
+		this.max_fan_cost = max_fan_cost;
+		this.min_fan_cost = min_fan_cost;
+	}
 	
 }
