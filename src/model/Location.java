a2f3e65e97bccb4e113bd5248ea240733947e878
Philip Dasler <philip.dasler@gmail.com>
2013-01-25 00:17:37 -0500

    Added constructor

--- src/model/Location.java
+++ src/model/Location.java
@@ -1,7 +1,7 @@
 package model;
 
-import java.sql.Time;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Location {
 	private ArrayList<Puzzle> puzzle_list;
@@ -14,6 +14,21 @@ public class Location {
 	private String image_file;
 	private String map_file;
 	
-	private Time time_open;
-	private Time time_closed;
+	private Date time_open;
+	private Date time_closed;
+	
+	public Location(ArrayList<Puzzle> puzzle_list, String address,
+			String notes, String restroom_description, String food_description,
+			String image_file, String map_file, Date time_open, Date time_closed) {
+		super();
+		this.puzzle_list = puzzle_list;
+		this.address = address;
+		this.notes = notes;
+		this.restroom_description = restroom_description;
+		this.food_description = food_description;
+		this.image_file = image_file;
+		this.map_file = map_file;
+		this.time_open = time_open;
+		this.time_closed = time_closed;
+	}
 }
--- src/model/Puzzle.java
+++ src/model/Puzzle.java
@@ -4,15 +4,17 @@ import java.sql.Time;
 import java.util.ArrayList;
 
 public class Puzzle {
+
+	private String name;
 	private String start_code;
 	
 	private String flavor_text;
 	
-	private int total_fan_value;
+	private int max_fan_worth;
 	private int current_fan_worth;
 	
 	private int elapsed_minutes;
-	private int par_minutes;
+	private int par_time;
 	private Time start_time;
 	private Time end_time;
 	private Time time_of_last_hint;
@@ -23,6 +25,35 @@ public class Puzzle {
 	
 	private ArrayList<Hint> hints;
 	private ArrayList<Answer> answers;
-	private ArrayList<Resource> resources_unlocked;
-	private ArrayList<Team> teams_killed;
+	private ArrayList<String> resources_unlocked;
+	private ArrayList<String> teams_killed;
+	
+	public Puzzle(String name, String start_code, String flavor_text,
+			int max_fan_worth, int par_time, ArrayList<Hint> hints,
+			ArrayList<Answer> answers, ArrayList<String> resources_unlocked,
+			ArrayList<String> teams_killed) {
+
+		this.name = name;
+		this.start_code = start_code;
+		
+		this.flavor_text = flavor_text;
+		
+		this.max_fan_worth = max_fan_worth;
+		this.current_fan_worth = max_fan_worth;
+		
+		this.elapsed_minutes = 0;
+		this.par_time = par_time;
+		this.start_time = new Time(0);
+		this.end_time = new Time(0);
+		this.time_of_last_hint = new Time(0);
+		
+		this.status = "unstarted";
+		
+		this.guesses_made = new ArrayList<Entry>();
+		
+		this.hints = hints;
+		this.answers = answers;
+		this.resources_unlocked = resources_unlocked;
+		this.teams_killed = teams_killed;
+	}
 }
