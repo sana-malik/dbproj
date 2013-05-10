ac74e7d96a8ee6a8bbb8841f4bbabfed0775b43a
Sana Malik <maliks@umd.edu>
2013-05-10 00:47:29 -0400

    i r stoops

--- experiments.py
+++ experiments.py
@@ -19,26 +19,28 @@ def validate(formula):
 	return solve(formula)
 
 def runExperiments():
-	filename = 'all-formulas';
+	filename = 'all-new-formulas';
 	f = open(filename+'.txt','r')
 	lines = [line.strip() for line in f.readlines()]
 	f.close()
-	f = open(filename+'-times.txt','w')
-	f.write('query_class,query_length,db_size,v_time,r_time\n')
-	
-	for line in lines:
-		(query_class,query_length,formula) = line.split("|")
-		v_time = timeit.Timer(stmt='solve("'+formula+'")', setup="from validator import solve").timeit(100)/100
-		for db_size in ['small','medium','large']:
-			query = convertToQuery(formula,db_size)
-			print "running %s,%s,%s\n\t%s" % (query_class,query_length,db_size,query)
-			stdout.flush()
-			try:
-				r_time = time_query(query)
-				f.write("%s,%s,%s,%f,%f\n" % (query_class,query_length,db_size,v_time,r_time))
-			except mysql.connector.Error as err:
-				print("Something went wrong: {}".format(err))
+	with open(filename+'-times.txt','w') as f:
+		f.write('query_class,query_length,db_size,v_time,r_time\n')
+		f.flush()
+		
+		for line in lines:
+			(query_class,query_length,formula) = line.split("|")
+			v_time = timeit.Timer(stmt='solve("'+formula+'")', setup="from validator import solve").timeit(100)/100
+			for db_size in ['small','medium','large']:
+				query = convertToQuery(formula,db_size)
+				print "running %s,%s,%s\n\t%s" % (query_class,query_length,db_size,query)
 				stdout.flush()
+				try:
+					r_time = time_query(query)
+					f.write("%s,%s,%s,%f,%f\n" % (query_class,query_length,db_size,v_time,r_time))
+					f.flush()
+				except mysql.connector.Error as err:
+					print("Something went wrong: {}".format(err))
+					stdout.flush()
 	
 if __name__ == "__main__":
 	runExperiments()
